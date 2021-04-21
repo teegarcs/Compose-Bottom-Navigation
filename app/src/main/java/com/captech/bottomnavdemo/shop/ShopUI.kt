1775d2a74bb9410c22db65dfeb1b30d7753ecf69
teegarcs <teegarcs@gmail.com>
2021-04-21 18:34:00 -0400

    Adding some constraint layout examples

--- app/src/main/java/com/captech/bottomnavdemo/shop/ShopUI.kt
+++ app/src/main/java/com/captech/bottomnavdemo/shop/ShopUI.kt
@@ -1,7 +1,12 @@
 package com.captech.bottomnavdemo.shop
 
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
@@ -13,7 +18,39 @@ fun ShopScreen() {
 
 @Composable
 fun BuildShop(state: ShopState) {
+    ConstraintLayout {
+        // Creates references for the three Composables
+        val (button1, button2, text) = createRefs()
 
+        Button(
+            // constraintAs is like setting the ID, required.
+            modifier = Modifier.constrainAs(button1) {
+                top.linkTo(parent.top, margin = 16.dp)
+            },
+            onClick = {}
+        ) {
+            Text("Button 1")
+        }
+
+        // constraintAs is like setting the ID, required.
+        Text("Text", Modifier.constrainAs(text) {
+            top.linkTo(button1.bottom, margin = 16.dp)
+            centerAround(button1.end)
+        })
+
+        // Create barrier to set the right button to the right of the
+        //button or the text, which ever is longer.
+        val barrier = createEndBarrier(button1, text)
+        Button(
+            modifier = Modifier.constrainAs(button2) {
+                top.linkTo(parent.top, margin = 16.dp)
+                start.linkTo(barrier)
+            },
+            onClick = {}
+        ) {
+            Text("Button 2")
+        }
+    }
 }
 
 @Composable
